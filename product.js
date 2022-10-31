35b279c94e479d4fa529f6196256e669a99dbc6f
CusMeDroid <108187260+CusMeDroid@users.noreply.github.com>
2022-11-01 04:09:16 +0700

    Update product.js

--- product.js
+++ product.js
@@ -1,3 +1,30 @@
+const mySwiper = new Swiper(".swiper-container", {
+  // Optional parameters
+  direction: "horizontal",
+  loop: true,
+  speed: 300,
+  mousewheel: true,
+  coverflowEffect: {
+    rotate: 30,
+    slideShadows: true
+  },
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination"
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev"
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: ".swiper-scrollbar"
+  }
+});
+
 let url = 'https://docs.google.com/spreadsheets/d/1-9Fb7S5YtfD8quHJdESZlbiBemK_5qd3tSm4exlkAog/gviz/tq?';
 const lkjs = document.querySelector('.lkjs');
 const query = encodeURIComponent('Select B,C,D,E,F,G');
